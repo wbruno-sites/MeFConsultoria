896b051a2fd8a5a6509d5b89a7e8c11e886c2ebd
William Bruno <rocha_bruno@hotmail.com>
2015-05-18 15:18:05 -0300

    Initial Commit

---
+++ app.js
@@ -0,0 +1,54 @@
+var express = require('express'),
+    bodyParser = require('body-parser'),
+    path    = require('path'),
+    favicon = require('serve-favicon'),
+    swig    = require('swig'),
+
+    app     = express();
+
+// view engine setup
+app.engine('html', swig.renderFile);
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'html');
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(__dirname + '/public/favicon.ico'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', require('./routes'));
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
+    });
+  });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
+});
+
+
+module.exports = app;
