13fd1e56eb3744f73effd735523f907dd74abf9a
William Bruno <rocha_bruno@hotmail.com>
2015-06-04 16:16:58 -0300

    New texts

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,16 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function(request, response, next) {
+  response.render('index');
+});
+
+router.get('/quem-somos', function(request, response, next) {
+  response.render('quem-somos');
+});
+
+router.get('/contato', function(request, response, next) {
+  response.render('contato');
 });
 
 module.exports = router;
